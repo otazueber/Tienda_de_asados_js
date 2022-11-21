e3002ba7a6bec475ef6db959418616e9c519059c
Eber <eber@axoft.com>
2022-11-21 17:52:11 -0300

    Tercera entrega del proyecto final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
+let desloguear = document.getElementById("desloguear");
+let producto = document.getElementById("producto");
+
+function leerCredenciales() {
+    let usuario = JSON.parse(sessionStorage.getItem("credenciales"));
+    if (usuario == null) {
+        document.getElementById("person").style.visibility = "visible";
+        desloguear.innerText = "";
+    } else {
+        document.getElementById("person").style.visibility = "hidden";
+        desloguear.innerText = "Cerrar seción";
+    }
+}
+
+leerCredenciales();
+
+function deslogearse() {
+    sessionStorage.clear();
+    document.getElementById("person").style.visibility = "visible";
+    desloguear.innerText = "";
+}
+
+desloguear.addEventListener("click", (e) => {
+    e.preventDefault();
+    deslogearse();
+}
+);
+
+function agregarAlCarrito() {
+    console.log("Agregado"); //a desarrollar para la entrega final
+}
+/* a desarrollar para la entrega final
+producto.addEventListener("click", (e) => {
+    e.preventDefault();
+    agregarAlCarrito();
+}
+);
+*/
+
+
+
+
+/*
 let user = "";
 let pass = "";
 let usuarioLogueado = false;
@@ -22,16 +65,14 @@ function loguear() {
             salir = true;
         }
     }
-    return usuarioLogueado;
 }
 
 function pedirCantidad(AConsiderarDecimales) {
-    let aux;
     if (AConsiderarDecimales) {
-        aux = parseFloat(prompt("Ingrese la cantidad"));
+        let aux = parseFloat(prompt("Ingrese la cantidad"));
     }
     else {
-        aux = parseInt(prompt("Ingrese la cantidad"));
+        let aux = parseInt(prompt("Ingrese la cantidad"));
     }
     if (aux == NaN) {
         aux = 0;
@@ -39,86 +80,28 @@ function pedirCantidad(AConsiderarDecimales) {
     return aux;
 }
 
-class Producto {
-    constructor(AId, ADescripcion, ACantidad, APrecio) {
-        this.id = AId;
-        this.descripcion = ADescripcion;
-        this.cantidad = ACantidad;
-        this.precio = APrecio;
-    }
-    ObtenerTotalProducto(){
-        return this.precio * this.cantidad;
-    }
+function Producto(ADescripcion, ACantidad, APrecio) {
+    this.descripcion = ADescripcion;
+    this.cantidad = ACantidad;
+    this.precio = APrecio;
 }
 
-function obtenerTotalPedido() {
+function obtenerTotal(){
     let total = 0;
-    productosSeleccionados.forEach((producto) => {
-        total = total + producto.ObtenerTotalProducto();
+    productosSeleccionados.forEach((producto)=>{
+        total = total + (precio.precio * producto.cantidad);
     }
     );
-    return total + ".";
+    return total;
 }
 
-function obtenerListaProductos() {
+function obtenerListaProductos(){
     let listaProductos = "";
-    productosSeleccionados.forEach((producto) => {
-        listaProductos = listaProductos + " * " + producto.descripcion + ": Cantidad = " + producto.cantidad + ", precio = " + producto.precio + ", Importe = " + (producto.precio * producto.cantidad) + ".\n";
+    productosSeleccionados.forEach((producto)=>{
+        listaProductos = listaProductos + precio.descripcion + " Cantidad: " + producto.cantidad + " precio: " + precio.precio + " Importe: " + (precio.precio * producto.cantidad) + "\n";
     }
     );
     return listaProductos;
-}
-
-function ProductoYaElegido(AId) {
-    const encontrado = productosSeleccionados.find((producto) => {
-        return producto.id === AId;
-    })
-    if (encontrado === undefined) {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
-function SumarCantidadAProducto(AId, ACantidad)
-{
-    const producto = productosSeleccionados.find((producto) => {
-        return producto.id === AId;
-    })
-    if (producto != undefined) {
-        producto.cantidad = producto.cantidad + ACantidad;
-    }
-}
-
-function EliminarProducto(AId)
-{
-    productosSeleccionados.splice(AId, 1);
-}
-
-function ProponerEliminacionProductos()
-{
-    let eliminaProducto = parseInt(prompt("¿Quiere verificar la lista para eliminar algún producto?\n\n1 - Si\n2 - No"));
-    if ((eliminaProducto != NaN) & (eliminaProducto == 1))
-    {
-        let Listado = "";
-        productosSeleccionados.forEach(element => {
-            Listado = Listado + element.id + " - " + element.descripcion + "\n";
-        });
-        let id = parseInt(prompt("Elija un producto a eliminar\n" + Listado));
-        if (id != NaN)
-        { 
-            for (let index = 0; index < productosSeleccionados.length; index++) {
-                const element = productosSeleccionados[index];
-                if (parseInt(element.id) == id)
-                {
-                    EliminarProducto(index);
-                    break;
-                }
-                
-            }
-        }
-    }
 }
 
 if (loguear()) {
@@ -126,34 +109,27 @@ if (loguear()) {
     let auxProducto = "";
     let auxCantidad = 0;
     let tieneProductos = false;
-    let producto;
+    let total = 0;
 
     while (seguirComprado) {
         auxProducto = prompt("Elija un producto: \n1 - Vacío ($ 1.550,00)\n2 - Entraña ($ 2.190,00)\n3 - Tapa de asado ($1.150,00)\n4 - Matambre ($1.290,00)\n5 - Finalizar");
         if ((auxProducto == "1") || (auxProducto == "2") || (auxProducto == "3") || (auxProducto == "4")) {
             auxCantidad = pedirCantidad(true);
             if (auxCantidad > 0) {
-                if (ProductoYaElegido(auxProducto))
-                {
-                    SumarCantidadAProducto(auxProducto, auxCantidad);
-                }
-                else{
-                    switch (auxProducto) {
+                switch (auxProducto) {
                     case "1":
-                        producto = new Producto("1", "Vacío", auxCantidad, 1550);
+                        productosSeleccionados.push(new Producto("Vacío", auxCantidad, 1550));
                         break;
                     case "2":
-                        producto = new Producto("2", "Entraña", auxCantidad, 2190);
+                        productosSeleccionados.push(new Producto("Entraña", auxCantidad, 2190));
                         break;
                     case "3":
-                        producto = new Producto("3", "Tapa de asado", auxCantidad, 1150);
+                        productosSeleccionados.push(new Producto("Tapa de asado", auxCantidad, 1150));
                         break;
                     case "4":
-                        producto = new Producto("4", "Matambre", auxCantidad, 1290);
+                        productosSeleccionados.push(new Producto("Matambre", auxCantidad, 1290));
                         break;
-                    }
-                    productosSeleccionados.push(producto);
-                }                
+                }
                 tieneProductos = tieneProductos || ((auxCantidad > 0));
             }
         } else if (auxProducto == "5") {
@@ -161,14 +137,8 @@ if (loguear()) {
         }
     }
     if (tieneProductos) {
-        ProponerEliminacionProductos();
-        if (productosSeleccionados.length > 0)
-        {
-            alert("Gracias por su pedido,\nSu lista de productos es la siguiente:\n" + obtenerListaProductos() + "El total a abonar es: " + obtenerTotalPedido());
-        }
-        else
-        {
-            alert("Su lista de productos está vacía.");
-        }
+        alert("Gracias por su pedido, el total a abonar es: " + obtenerTotal() + "\nSu lista de productos es la siguiente:\n" + obtenerListaProductos());
     }
+
 }
+*/
